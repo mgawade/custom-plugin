7ec1f55cff6c9ebcd9f8554938aa0c8c6768007b
Mayur Ankush Gawade <mgawade@mobiquityinc.com>
2019-11-26 17:52:53 +0530

    Created first plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HelloWorld } from 'hello-world';
+
 
 @NgModule({
   declarations: [
@@ -31,6 +33,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    HelloWorld,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HelloWorld } from '@ionic-native/plugins/hello-world';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
-
-  }
+  constructor(public helloWorld: HelloWorld) {
+    this.helloWorld.coolMethod({
+      _sMessage: "Hello World"
+    }).then((result) => {
+      alert(JSON.stringify(result, undefined, 2));
+    }, (err) => {
+      alert(JSON.stringify(err, undefined, 2));
+    });  }
 }
